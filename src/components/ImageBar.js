b26fc8c73345e925b4a212a54dfdb2cc591a09ed
JAIP1003 <jgatep@gmail.com>
2020-08-02 01:09:14 +0530

    first commit

---
+++ src/components/ImageBar.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+
+import './ImageBarCss.css';
+
+function ImageBar(){
+    return(
+        <>
+        <div className="container-fluid">
+            <div className="row">
+                <div  className="col text-center jai1 bg-success">
+                    Happy Friendship Day 2020
+                    <img src="https://i.ibb.co/LC7MLcL/3.jpg" alt="HappyFriendshipDay"></img>
+                </div>
+            </div>
+        <div className="row">
+            <div className="col text-center jai">
+            HAPPY FRIENDSHIP DAY 2020
+            </div>
+        </div>
+        <div className="row">
+                <div  className="col text-center jai1 bg-primary">
+                    <img src="https://i.ibb.co/CK63q4f/2.jpg" alt="HappyFriendshipDay"></img>
+                    Made With Love ❤️ In Patna
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default ImageBar;
+
+/*https://i.ibb.co/LC7MLcL/3.jpg
+
+https://i.ibb.co/CK63q4f/2.jpg
+
+https://i.ibb.co/jz6mcpn/5.webp
+
+https://i.ibb.co/xSPTPQd/8.jpg
+
+*/
