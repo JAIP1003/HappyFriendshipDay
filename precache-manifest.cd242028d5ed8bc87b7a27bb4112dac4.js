28f8b869faca77af0911a41cf59e225093f63399
JAIP1003 <jgatep@gmail.com>
2020-08-02 01:10:52 +0530

    Updates

---
+++ precache-manifest.cd242028d5ed8bc87b7a27bb4112dac4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5396d77e9c458f5c94bd2d2fb79e59fa",
+    "url": "/HappyFriendshipDay/index.html"
+  },
+  {
+    "revision": "15a4e1e1fe7387e117cc",
+    "url": "/HappyFriendshipDay/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e030bd8636dc27b103bd",
+    "url": "/HappyFriendshipDay/static/css/main.a364cd13.chunk.css"
+  },
+  {
+    "revision": "15a4e1e1fe7387e117cc",
+    "url": "/HappyFriendshipDay/static/js/2.b1485d2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HappyFriendshipDay/static/js/2.b1485d2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e030bd8636dc27b103bd",
+    "url": "/HappyFriendshipDay/static/js/main.87201c68.chunk.js"
+  },
+  {
+    "revision": "f14207180b640cd4a4f6",
+    "url": "/HappyFriendshipDay/static/js/runtime-main.793130ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HappyFriendshipDay/static/media/logo.5d5d9eef.svg"
+  }
+]);
